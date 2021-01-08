04beb5da5de9922494c3c0e44c7ac66d7cbc97f1
Faybeee <76427698+Faybeee@users.noreply.github.com>
2021-01-08 21:20:42 +0000

    Add files via upload

---
+++ session1 task1 homework.py
@@ -0,0 +1,35 @@
+import random
+
+number: int = random.randint(0, 10)
+
+
+print(number)
+
+print("Hello, I am a random number guessing game!")
+user_input = input ("what is your name?")
+
+print("Hello", user_input, "lets play a game!  I'm thinking of a number between 0 and 10, try and guess what it is!")
+
+guess = int
+while guess != number:
+
+    guess = int(input("What number do you guess?"))
+
+    if guess < number:
+        print("Too low, try again.")
+
+    elif guess > number:
+        print("Too high, try again.")
+
+if guess == number:
+    print("Smarty pants! You guessed correctly! The number was", number, "!")
+
+
+
+
+
+
+
+
+
+
